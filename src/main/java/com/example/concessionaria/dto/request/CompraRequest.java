cdb1a76fd33abd91621ca9556c00cf040d42328e
Cristopher <cristopherspqr@gmail.com>
2025-11-18 16:11:21 -0300

    Criação dos repository e dto de compra e pedido

---
+++ src/main/java/com/example/concessionaria/dto/request/CompraRequest.java
@@ -0,0 +1,15 @@
+package com.example.concessionaria.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record CompraRequest(
+
+        @NotNull(message = "O ID do automóvel é obrigatório")
+        Long automovelId,
+
+        @NotNull(message = "O ID do cliente é obrigatório")
+        Long clienteId,
+
+        @NotNull(message = "O ID do vendedor é obrigatório")
+        Long vendedorId
+) {}
